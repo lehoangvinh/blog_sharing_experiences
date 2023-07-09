b36b3d355ff42817e831e4322dd4a5423a62af76
lehoangvinh <lhv.vinh88@gmail.com>
2023-07-09 22:48:08 +0700

    fix 2

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,29 +1,28 @@
-"use client";
-import React from "react";
 import "@styles/global.css";
+
 import Navbar from "@components/Navbar";
 import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopia",
-  description: "Discover & share AI Promps",
+  description: "Discover & Share AI Prompts",
 };
-const RootLayout = ({ children }) => {
-  return (
-    <html lang="en">
-      <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient"></div>
-          </div>
-          <main className="app">
-            <Navbar />
-            {children}
-          </main>
-        </Provider>
-      </body>
-    </html>
-  );
-};
+
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+          <Navbar />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
